a9e00c12813a79d7550bbc0dbddcf938aeb91ae4
HawraaYounes <hawraayounes0@gmail.com>
2024-05-16 11:55:12 +0300

    updated state immutabele

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,16 +1,34 @@
+import { useState } from "react";
+
 const GameBoard = () => {
   const initialGameBoard = [
     [null, null, null],
     [null, null, null],
     [null, null, null],
   ];
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  function handleClick(rowIndex, colIndex) {
+    console.log(rowIndex,colIndex)
+    setGameBoard((prevGameBoard) => {
+      const updatedBoard = [...prevGameBoard.map(innerArray=>[...innerArray])];
+      updatedBoard[rowIndex][colIndex]='X';
+      console.log(updatedBoard)
+      return updatedBoard;
+    });
+  }
+
   return (
     <ol id="game-board">
-      {initialGameBoard.map((row, index) => (
-        <li key={index}>
+      {gameBoard.map((row, rowIndex) => (
+        <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
-              <li key={colIndex}><button>{playerSymbol}</button></li>
+              <li key={colIndex}>
+                <button onClick={() => handleClick(rowIndex, colIndex)}>
+                  {playerSymbol}
+                </button>
+              </li>
             ))}
           </ol>
         </li>
